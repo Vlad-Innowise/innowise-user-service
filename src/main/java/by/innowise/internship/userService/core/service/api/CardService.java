674028676b0c21781e10a4907445af9086089be9
Vlad Golubev <holybay1886@gmail.com>
2025-07-09 23:15:56 +0300

    Added and impl a method to create a new card for the user in the CardService.

--- src/main/java/by/innowise/internship/userService/core/service/api/CardService.java
+++ src/main/java/by/innowise/internship/userService/core/service/api/CardService.java
@@ -1,6 +1,11 @@
 package by.innowise.internship.userService.core.service.api;
 
+import by.innowise.internship.userService.api.dto.cardInfo.CardInfoCreateDto;
+import by.innowise.internship.userService.api.dto.cardInfo.CardInfoResponseDto;
+
 public interface CardService {
+
+    CardInfoResponseDto create(CardInfoCreateDto dto, Long userId);
 
     boolean cardNumberExists(String number);
 
--- src/main/java/by/innowise/internship/userService/core/service/impl/CardServiceImpl.java
+++ src/main/java/by/innowise/internship/userService/core/service/impl/CardServiceImpl.java
@@ -1,6 +1,10 @@
 package by.innowise.internship.userService.core.service.impl;
 
+import by.innowise.internship.userService.api.dto.cardInfo.CardInfoCreateDto;
+import by.innowise.internship.userService.api.dto.cardInfo.CardInfoResponseDto;
+import by.innowise.internship.userService.core.mapper.CardInfoMapper;
 import by.innowise.internship.userService.core.repository.CardInfoRepository;
+import by.innowise.internship.userService.core.repository.entity.CardInfo;
 import by.innowise.internship.userService.core.service.api.CardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class CardServiceImpl implements CardService {
 
     private final CardInfoRepository cardRepository;
+    private final CardInfoMapper mapper;
+
+    @Transactional
+    @Override
+    public CardInfoResponseDto create(CardInfoCreateDto dto, Long userId) {
+        CardInfo saved = cardRepository.saveAndFlush(mapper.toEntity(dto, userId));
+        log.info("Saved a card info: {}", saved);
+        CardInfoResponseDto responseDto = mapper.toDto(saved);
+        log.info("Returning a response dto: {}", responseDto);
+        return responseDto;
+    }
 
     @Transactional(readOnly = true)
     @Override

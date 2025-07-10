fb62cc79e4576659d76a75fee8824565c8040773
Vlad Golubev <holybay1886@gmail.com>
2025-07-11 01:04:22 +0300

    Fix: Changed FetchType to Lazy for the CardInfo entity.

--- src/main/java/by/innowise/internship/userService/core/repository/entity/CardInfo.java
+++ src/main/java/by/innowise/internship/userService/core/repository/entity/CardInfo.java
@@ -2,6 +2,7 @@ package by.innowise.internship.userService.core.repository.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -40,7 +41,7 @@ public class CardInfo extends BaseEntity {
     private LocalDate expirationDate;
 
     @ToString.Exclude
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 

0affe0a768afe2c74c2d352686064b7651b07315
Vlad Golubev <holybay1886@gmail.com>
2025-07-14 00:17:41 +0300

    Removed @Validated annotation from AdminController.

--- src/main/java/by/innowise/internship/userService/api/controller/AdminController.java
+++ src/main/java/by/innowise/internship/userService/api/controller/AdminController.java
@@ -5,7 +5,6 @@ import by.innowise.internship.userService.core.service.api.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +15,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@Validated
 @RequestMapping("/api/admin")
 public class AdminController {
 
